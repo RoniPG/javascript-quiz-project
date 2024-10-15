6594d7d84ecc8f1bf34cadc300a1fb8268c57111
RoniPG <ronigmn@gmail.com>
2024-10-15 11:44:45 +0200

    Day 1

--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,45 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
-
+    constructor(questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
     // 2. getQuestion()
-    
+    getQuestion() {
+        return this.questions[this.currentQuestionIndex];
+    }
     // 3. moveToNextQuestion()
-
+    moveToNextQuestion() {
+        this.currentQuestionIndex++;
+    }
     // 4. shuffleQuestions()
+    shuffleQuestions() {
+        // Método de Fisher-Yates
+        if (!this.questions) {
+            return undefined;
+        }
+        for (let i = this.questions.length - 1; i > 0; i--) {
+            // Seleccionar un elemento aleatorio
+            const m = Math.floor(Math.random() * (i + 1));
 
+            // Intercambiar el elemento actual con el elemento seleccionado
+            [this.questions[i], this.questions[m]] = [this.questions[m], this.questions[i]];
+        }
+
+        return this.questions;
+    }
     // 5. checkAnswer(answer)
-
+    checkAnswer(answer) {
+        if (answer === this.getQuestion().answer) {
+            this.correctAnswers ++;
+        } 
+    }
     // 6. hasEnded()
+    hasEnded() {
+        return this.currentQuestionIndex === this.questions.length;
+    }
 }
